be44b3364a42556febae369484b7463046dba6c5
unknown <chinmaylokhande30@gmail.com>
2025-06-25 15:28:29 +0530

    vercel server updated in server.js

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ connectDB();
 const app  = express();
 
 const allowedOrigins = [
-  'https://blog-verse.vercel.app', 
+  'https://blog-verse-xi.vercel.app', 
   'http://localhost:5173',         
 ];
 
